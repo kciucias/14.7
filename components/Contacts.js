555aa4ec9201954c9a957d8d8a16cefc23aecd1e
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-08-03 23:22:09 +0100

    Add JSX

--- components/Contacts.js
+++ components/Contacts.js
@@ -3,14 +3,14 @@ var Contacts = React.createClass({
   propTypes: {
     items: React.PropTypes.array.isRequired,
   },
-// map() bierze element z tablicy this.props.items (odwolanie do propsa o nazwie item z komponentu contact.js) Czym jest contact?
+
   render: function() {
     var contacts = this.props.items.map(function(contact) {
-        return React.createElement(Contact, {item: contact, key: contact.id});
-    }); // {item: contact, key: contact.id} ??
+        return <Contact item={contact} key={contact.id}>
+    }); 
 
     return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
+      <ul className={'contactsList'}> {contacts}</ul>
     );
   }
 });
